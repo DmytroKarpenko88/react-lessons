e4e177c8d9d91cb5ad7ef6183b37dae5d3afcba2
Dmytro Karpenko <kardmitriy@gmail.com>
2023-04-22 21:59:57 +0300

    event board

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { PageTitle } from 'components/PageTitile/PageTitle';
 import { EventsBoard } from 'components/EventBoard/EventBoard';
 
+import upcomingEvents from 'upcoming-events.json';
+
 export const App = () => {
   return (
-    <div>
+    <>
       <PageTitle text="24th Core Worlds Coalition Conference" />
-      <EventsBoard />
-    </div>
+      <EventsBoard events={upcomingEvents} />
+    </>
   );
 };
--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,5 +1,37 @@
+import PropTypes from 'prop-types';
+
+import { Event } from 'components/Event/Event';
 import css from './EventBoard.module.css';
 
-export const EventsBoard = () => {
-  return <div className={css.eventBoard}>Event catds</div>;
+export const EventsBoard = ({ events }) => {
+  return (
+    <div className={css.eventBoard}>
+      {events.map(event => (
+        <Event
+          key={event.name}
+          name={event.name}
+          location={event.location}
+          speaker={event.speaker}
+          type={event.type}
+          start={event.time.start}
+          end={event.time.end}
+        />
+      ))}
+    </div>
+  );
+};
+
+EventsBoard.propTypes = {
+  events: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      speaker: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      time: PropTypes.exact({
+        start: PropTypes.string.isRequired,
+        end: PropTypes.string.isRequired,
+      }),
+    })
+  ),
 };
